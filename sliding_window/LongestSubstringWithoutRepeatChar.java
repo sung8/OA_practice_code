8d4c62c6e9a08d5c559188fa4d6ec3398df3e97b
sung8 <heechuw@gmail.com>
2024-05-29 14:45:38 -0400

    sliding window - longest substring without repeating characters

---
+++ sliding_window/LongestSubstringWithoutRepeatChar.java
@@ -0,0 +1,166 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatChar {
+    public static int lengthOfLongestSubstring5(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        HashSet<Character> hs = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+        String longest = "";
+        String currStr = "";
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            while (hs.contains(c)) {
+                hs.remove(s.charAt(left));
+                currStr = currStr.substring(1);
+                left++;
+            }
+            hs.add(c);
+            currStr += c;
+            if (currStr.length() > longest.length()) {
+                longest = currStr;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            System.out.println(currStr);
+            System.out.println(" * " + longest);
+        }
+        return maxLength;
+    }
+
+    public static int lengthOfLongestSubstring4(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        HashSet<Character> hs = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            while (hs.contains(c)) {
+                hs.remove(s.charAt(left));
+                left++;
+            }
+            hs.add(c);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+
+    // for test case 1 "abcabcbb", StringIndexOutOfBoundsException
+    public static int lengthOfLongestSubstring3(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        HashSet<Character> hs = new HashSet<>();
+        int j = 0;
+        String longest = "";
+        String currStr = "";
+        while (j < s.length()) {
+            currStr += s.charAt(j);
+            if (hs.add(s.charAt(j)) == false) {
+                int i = 0;
+                while (i < j && currStr.charAt(i) != s.charAt(j)) { // index out of bounds exception
+                    hs.remove(currStr.charAt(i));
+                    currStr = currStr.substring(1);
+                    i++;
+                }
+                currStr = currStr.substring(1);
+            } else {
+                hs.add(s.charAt(j));
+            }
+            if (currStr.length() > longest.length()) {
+                longest = currStr;
+            }
+            System.out.println(currStr);
+            System.out.println(" * " + longest);
+            j++;
+        }
+        return longest.length();
+    }
+
+    // 60 / 987 testcases passed
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        HashSet<Character> hs = new HashSet<>();
+        int j = 1;
+        String longest = "";
+        String currStr = "";
+        while (j < s.length()) {
+            currStr += s.charAt(j);
+            if (hs.add(s.charAt(j)) == false) {
+                currStr = currStr.substring(1);
+            } else {
+                hs.add(s.charAt(j));
+            }
+            if (currStr.length() > longest.length()) {
+                longest = currStr;
+            }
+            System.out.println(currStr);
+            System.out.println(" * " + longest);
+            j++;
+        }
+        return longest.length();
+    }
+
+    // 407 / 987 testcases passed
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        // character, index in string
+        String longest = "";
+        String currStr = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!hm.containsKey(s.charAt(i))) {
+                currStr += s.charAt(i);
+                System.out.println(" " + currStr);
+                hm.put(s.charAt(i), currStr.length() - 1);
+                if (currStr.length() > longest.length()) {
+                    longest = currStr;
+                }
+            } else {
+                currStr = "" + s.charAt(i);
+                hm.clear();
+                hm.put(s.charAt(i), 0);
+                System.out.println("Hello " + currStr);
+            }
+        }
+        return longest.length();
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcabcbb";
+        String str2 = "bbbbb";
+        String str3 = "pwwkew";
+        String str407 = "dvdf";
+        // System.out.println(lengthOfLongestSubstring(str407));
+        // System.out.println(lengthOfLongestSubstring2(str407));
+
+        // next attempt
+        // System.out.println(lengthOfLongestSubstring3(str3));
+        // System.out.println(lengthOfLongestSubstring3(str407));
+        // System.out.println(lengthOfLongestSubstring3(" "));
+        // System.out.println(lengthOfLongestSubstring3(str1)); //
+        // StringIndexOutOfBoundsException
+
+        // Test 1
+        // System.out.println(lengthOfLongestSubstring4(str1));
+        // System.out.println(lengthOfLongestSubstring5(str1));
+        // Test 2
+        // System.out.println(lengthOfLongestSubstring4(str3));
+        // System.out.println(lengthOfLongestSubstring5(str3));
+        // Test 407
+        System.out.println(lengthOfLongestSubstring4(str407));
+        System.out.println(lengthOfLongestSubstring5(str407));
+    }
+}
